a23296ccb87ef450edc74ad67e2c6efbafdde8fd
Lowtea212014 <75553487+Lowtea212014@users.noreply.github.com>
2021-02-21 17:01:56 +0800

    20210221

--- 3D_FPS/Assets/Script/FPScontroller.cs
+++ 3D_FPS/Assets/Script/FPScontroller.cs
@@ -17,7 +17,17 @@ public class FPScontroller : MonoBehaviour
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        OnAnimatorMove();
+    }
+    ///移動
+    private void OnAnimatorMove()
+    {
 
-
+        float v = Input.GetAxis("Vertical"); // W S 上下- 前進1 後退-1
+        float h = Input.GetAxis("Horizontal"); //A D 左右
+        rig.MovePosition(transform.position + transform.forward * v * speed * Time.deltaTime+transform.right*h*speed*Time.deltaTime);
+    }
 
 }
