0164208f8b8bb0880569d2720bf28a91f842aa96
Lowtea212014 <75553487+Lowtea212014@users.noreply.github.com>
2021-02-21 16:40:15 +0800

    20210221

---
+++ 3D_FPS/Assets/Script/FPScontroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FPScontroller : MonoBehaviour
+{
+    //頭銜
+    [Header("移動"), Range(0, 2000)]
+    public float speed;
+    [Header("旋轉"), Range(0, 2000)]
+    public float turn;
+
+    //欄位
+    private Animator ani;
+    private Rigidbody rig;
+    //取得
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+    }
+
+
+
+}
